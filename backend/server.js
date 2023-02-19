3de259d6fbb9cf820bfe27ffabb9a0666bef6ca8
LesserQuire <117785103+LesserQuire@users.noreply.github.com>
2023-02-18 23:32:07 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
+app.put('/', (req, res) => {
 	let input = req.body;
 	res.send(identify(input['street'],input['city']))
 })
