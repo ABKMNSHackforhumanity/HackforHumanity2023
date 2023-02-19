69a2c7ae89700668869355327faf2d3be1008861
LesserQuire <117785103+LesserQuire@users.noreply.github.com>
2023-02-18 18:55:07 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const fetch = require("node-fetch");
 const json = require("json");
 
-var url
+var street;
+var city;
+
+var url = "https://geocoding.geo.census.gov/geocoder/geographies/address?street=" 
+	+ street + "&city=" + city 
+	+ "&state=CA&benchmark=Public_AR_Census2020&vintage=Census2020_Census2020&layers=10&format=json";
 
 var requestOptions = {
     Method: 'GET'
@@ -13,5 +18,5 @@ var output = fetch(url, requestOptions)
 	.then(response => response.json())
 	.then(result => handleOutput(result))
 	//catch all errors to not kill all
-	.catch(error => console.log('error', error))
+	.catch(error => console.log('error', error));
 
