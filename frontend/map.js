ae3d4100e234b7d51c36477da7a5228f8eafdb95
Bryce-7034 <kellyb@wlhs.wlwv.k12.or.us>
2023-02-18 18:03:55 -0800

    added a basic react.js file to start

---
+++ frontend/map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class map extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello Wordle!</h1>
+            </div>
+        );
+    }
+
+}
